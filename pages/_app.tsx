882e83bb919f4d30c4cfcdb83ca4e2015c5fad5f
Himanshu <himanshurahul1224@gmail.com>
2024-02-12 02:11:18 +0530

    Added Authentication flow in FE

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,25 @@
 import "@/styles/globals.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import type { AppProps } from "next/app";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Inter } from "next/font/google";
 import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const queryClient = new QueryClient();
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className={inter.className}>
-      <GoogleOAuthProvider clientId="1069572055036-n0mm4dgjrt993ed1ptff2kg4aimpm0sr.apps.googleusercontent.com">
-        <Component {...pageProps} />
-        <Toaster />
-      </GoogleOAuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <GoogleOAuthProvider clientId="1069572055036-n0mm4dgjrt993ed1ptff2kg4aimpm0sr.apps.googleusercontent.com">
+          <Component {...pageProps} />
+          <Toaster />
+          <ReactQueryDevtools />
+        </GoogleOAuthProvider>
+      </QueryClientProvider>
     </div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -15,6 +15,8 @@ import { CredentialResponse, GoogleLogin } from "@react-oauth/google";
 import toast from "react-hot-toast";
 import { graphqlClient } from "@/clients/api";
 import { verifyUserGoogleTokenQuery } from "@/graphql/query/user";
+import { useCurrentUser } from "@/hooks/user";
+import { useQueryClient } from "@tanstack/react-query";
 
 interface TwitterSidebarButton {
   title: string;
@@ -35,6 +37,10 @@ const sidebarMenuItems: TwitterSidebarButton[] = [
 ];
 
 export default function Home() {
+  const { user } = useCurrentUser();
+
+  const queryClient = useQueryClient();
+
   const handleLoginWithGoogle = useCallback(
     async (cred: CredentialResponse) => {
       const googleToken = cred.credential;
@@ -50,15 +56,19 @@ export default function Home() {
 
       if (verifyGoogleToken) {
         window.localStorage.setItem("__twitter_token", verifyGoogleToken);
+
+        //previously below code used to work like -> await queryClient.invalidateQueries(["current-user"]);
+
+        await queryClient.invalidateQueries({ queryKey: ["current-user"] });
       }
     },
-    []
+    [queryClient]
   );
 
   return (
     <div>
       <div className="grid grid-cols-12 h-screen w-screen px-32">
-        <div className="col-span-3 ">
+        <div className="col-span-3 static">
           <div className="text-3xl w-fit hover:bg-gray-900 p-3 rounded-full cursor-pointer transition-all">
             <BsTwitterX />
           </div>
@@ -74,10 +84,27 @@ export default function Home() {
                 </li>
               ))}
             </ul>
-            <button className="bg-[#1d9bf0] p-4 rounded-full w-9/12 mt-2 text-base font-bold">
+            <button className="bg-[#1d9bf0] p-4 rounded-full w-9/12 mt-2 text-[16px] font-bold hover:bg-[#1d9cf0e0]">
               Post
             </button>
           </div>
+          {user && (
+            <div className="absolute bottom-1 flex gap-2 items-center p-2 rounded-full hover:bg-gray-900 cursor-pointer transition-all">
+              {user && user.profileImageUrl && (
+                <Image
+                  className="rounded-full"
+                  src={user?.profileImageUrl}
+                  alt="user-image"
+                  height={50}
+                  width={50}
+                />
+              )}
+              <div className="flex">
+                <h3 className="text-xl">{user.firstName}</h3>
+                <h3 className="text-xl">{user.lastName}</h3>
+              </div>
+            </div>
+          )}
         </div>
         <div className="col-span-5 border-r border-l border-gray-700 -ml-12">
           <FeedCard />
@@ -88,10 +115,12 @@ export default function Home() {
           <FeedCard />
         </div>
         <div className="col-span-3 p-4">
-          <div className="p-4 border rounded-lg ">
-            <h1 className="my-2 text-3xl font-bold">Join today.</h1>
-            <GoogleLogin onSuccess={handleLoginWithGoogle} />
-          </div>
+          {!user && (
+            <div className="p-4 border rounded-lg ">
+              <h1 className="my-2 text-3xl font-bold">Join today.</h1>
+              <GoogleLogin onSuccess={handleLoginWithGoogle} />
+            </div>
+          )}
         </div>
       </div>
     </div>
